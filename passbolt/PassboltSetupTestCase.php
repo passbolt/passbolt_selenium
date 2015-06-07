e2fc6e5c830876e21e2f9e0bb1e05fcef952f0e9
Kevin Muller <mynameisgnu@gmail.com>
2015-06-07 16:02:25 +0530

    #PASSBOLT-988 specialized PassboltSetupTestCase

---
+++ passbolt/PassboltSetupTestCase.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PassboltSetup Test Case
+ * A specialized class to test the setup of passbolt.
+ * It contains a set of functions useful to test specific elements of the setup.
+ *
+ * @copyright 	(c) 2015-present Bolt Software Pvt. Ltd.
+ * @licence			GPLv3 onwards www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
+
+class PassboltSetupTestCase extends PassboltTestCase {
+
+	/**
+	 * Assert that the title should be as given.
+	 * @param $title
+	 */
+	public function assertTitleShouldBe($title) {
+
+	}
+}
