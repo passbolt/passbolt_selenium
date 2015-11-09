34af867901f32cac46e296d613bac5545f112fce
Cedric Alfonsi <cedricalfonsi@gmail.com>
2015-11-09 20:48:34 +0100

    PASSBOLT-1140 PasswordWorkspaceTest::testPasswordDetails

--- tests/LU/base/PasswordWorkspaceTest.php
+++ tests/LU/base/PasswordWorkspaceTest.php
@@ -207,7 +207,7 @@ class PasswordWorkspaceTest extends PassboltTestCase
         $pwdDetails = [
             'username'        => 'vector',
             'url'             => 'https://inkscape.org/',
-            'modified'        => '/[0-9]{1,2} (days|hours|years) ago/',
+            'modified'        => '/([0-9]{1,2}|a|an) (day|hour|year)[s]? ago/',
             'created-by'      => 'edith@passbolt.com',
             'modified-by'     => 'anonymous@passbolt.com',
         ];
