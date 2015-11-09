53e08ec180b1657537b478c911d42ee62b6cb6d9
Cedric Alfonsi <cedricalfonsi@gmail.com>
2015-11-09 20:36:22 +0100

    PASSBOLT-1140 Fix PASSBOLT1103

--- tests/LU/regressions/PASSBOLT-1103_ContextualMenuDisappearAfterChangingWorkspaceTest.php
+++ tests/LU/regressions/PASSBOLT-1103_ContextualMenuDisappearAfterChangingWorkspaceTest.php
@@ -24,6 +24,7 @@ class PASSBOLT1103 extends PassboltTestCase {
 		$user = User::get('ada');
 
 		// And I am logged in on the password workspace
+		$this->setClientConfig($user);
 		$this->loginAs($user);
 
 		// When I right click on a password I own
