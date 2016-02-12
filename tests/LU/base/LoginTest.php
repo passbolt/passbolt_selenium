f5be29e36adf0f25743cb343a26da17e9a3332d3
Kevin Muller <mynameisgnu@gmail.com>
2016-02-12 18:10:21 +0530

    PASSBOLT-1300 selenium test for I should be able to login by pressing enter

--- tests/LU/base/LoginTest.php
+++ tests/LU/base/LoginTest.php
@@ -50,4 +50,44 @@ class LoginTest extends PassboltTestCase {
 	    );
     }
 
+	public function testLoginWithEnterKey() {
+		$this->getUrl('login');
+		sleep(1);
+		$this->assertVisible('.plugin-check.firefox.warning');
+
+		$user = User::get('ada');
+		$this->setClientConfig($user);
+
+		$this->getUrl('login');
+
+		$this->waitUntilISee('.plugin-check.firefox.success');
+		$this->waitUntilISee('.plugin-check.gpg.success');
+
+		$this->assertVisible('passbolt-iframe-login-form');
+		$this->goIntoLoginIframe();
+
+		$this->assertVisible('.login-form.master-password');
+		$this->assertInputValue('UserUsername', $user['Username']);
+
+		$this->click('js_master_password');
+		$this->typeTextLikeAUser($user['MasterPassword']);
+		$this->pressEnter();
+
+		$this->assertElementContainsText('loginMessage','Please wait');
+		$this->goOutOfIframe();
+
+		$this->waitUntilISee('.login.form .feedback');
+		$this->assertElementContainsText('.login.form .feedback','Logging in');
+		$this->waitCompletion();
+
+		// wait for redirection trigger
+		sleep(1);
+		$this->waitCompletion();
+
+		$this->assertElementContainsText(
+			$this->findByCss('.header .user.profile .details .name'),
+			'Ada Lovelace'
+		);
+	}
+
 }
