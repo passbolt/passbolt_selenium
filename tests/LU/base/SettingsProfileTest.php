b3e19201f35b1099eab5ff73759d26408117e21a
Kevin Muller <mynameisgnu@gmail.com>
2016-03-18 20:11:50 +0530

    PASSBOLT-1243 fixed test

--- tests/LU/base/SettingsProfileTest.php
+++ tests/LU/base/SettingsProfileTest.php
@@ -74,13 +74,13 @@ class SettingsProfileTest extends PassboltTestCase {
 		// And I should the created date in the ago format
 		$this->assertElementContainsText(
 			$this->find('.table-info.profile .created'),
-			'/(a|[0-9]{1,2}) (minute|minutes) ago/'
+			'/(a|[0-9]{1,2}) (seconds|minute|minutes) ago/'
 		);
 
 		// And I should see the correct modified date in the ago format
 		$this->assertElementContainsText(
 			$this->find('.table-info.profile .modified'),
-			'/(a|[0-9]{1,2}) (minute|minutes) ago/'
+			'/(a|[0-9]{1,2}) (seconds|minute|minutes) ago/'
 		);
 
 		// I should see the picture of Ada.
