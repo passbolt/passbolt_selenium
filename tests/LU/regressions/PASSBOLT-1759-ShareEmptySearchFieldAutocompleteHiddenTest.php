a0daa8ebc450f673ba5f076c948bedf54c4a224a
Cedric Alfonsi <cedricalfonsi@gmail.com>
2016-11-29 16:44:30 +0100

    PASSBOLT-1759 Share: autocomplete list will appear even when there is no text entered in the search

--- tests/LU/regressions/PASSBOLT-1759-ShareEmptySearchFieldAutocompleteHiddenTest.php
+++ tests/LU/regressions/PASSBOLT-1759-ShareEmptySearchFieldAutocompleteHiddenTest.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Bug PASSBOLT-1758 - Regression test
+ * Bug PASSBOLT-1759 - Regression test
  *
  * @copyright (c) 2015-present Bolt Softwares Pvt Ltd
  * @licence GNU Affero General Public License http://www.gnu.org/licenses/agpl-3.0.en.html
  */
-class PASSBOLT1758 extends PassboltTestCase {
+class PASSBOLT1759 extends PassboltTestCase {
 
 	/**
 	 * Scenario: As a user I can share a password with other users
@@ -13,9 +13,10 @@ class PASSBOLT1758 extends PassboltTestCase {
 	 * Given    I am Carol
 	 * And      I am logged in on the password workspace
 	 * When     I go to the sharing dialog of a password I own
-	 * And		I search a user by his lastname
-	 * Then 	I should see only one result
-	 * And 		I should see only the user Edit Clarke in the autocomplete list
+	 * And      I search a user
+	 * Then     I should see results
+	 * When 	I empty the search field
+	 * Then     the autocomplete field should be hidden
 	 */
 	public function testShareSearchUsersFiltersOnName() {
 		// Reset database at the end of test.
@@ -35,24 +36,29 @@ class PASSBOLT1758 extends PassboltTestCase {
 		));
 		$this->gotoSharePassword(Uuid::get('resource.id.apache'));
 
-		// And I search a user by his lastname
-		$userC = User::get('edith');
+		// And I search a user
 		$this->goIntoShareIframe();
-		$this->inputText('js_perm_create_form_aro_auto_cplt', $userC['LastName'], true);
+		$this->inputText('js_perm_create_form_aro_auto_cplt', '@passbolt.com', true);
 		$this->click('.security-token');
 		$this->goOutOfIframe();
 
 		// I wait the autocomplete box is loaded.
 		$this->waitCompletion(10, '#passbolt-iframe-password-share-autocomplete.loaded');
 
-		// Then I should see only one result
+		// Then I should see results
 		$this->goIntoShareAutocompleteIframe();
 		$listOfUsers = $this->driver->findElements(WebDriverBy::cssSelector('ul li'));
-		$this->assertEquals(1, count($listOfUsers));
+		$this->assertNotEquals(0, count($listOfUsers));
+		$this->goOutOfIframe();
 
-		// And I should see only the user Edit Clarke in the autocomplete list
-		$shareWithUserFullName = $userC['FirstName'] . ' ' . $userC['LastName'];
-		$this->waitUntilISee('.autocomplete-content', '/' . $shareWithUserFullName . '/i');
+		// When I empty the search field
+		$this->goIntoShareIframe();
+		$this->emptyFieldLikeAUser('js_perm_create_form_aro_auto_cplt');
+		$this->click('.security-token');
+		$this->goOutOfIframe();
+
+		// Then the autocomplete field should be hidden
+		$this->waitUntilIDontSee('#passbolt-iframe-password-share-autocomplete');
 	}
 
 }
