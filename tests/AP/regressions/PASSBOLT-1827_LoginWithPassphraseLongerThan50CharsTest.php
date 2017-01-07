b41205a1de29219772d886d2a73769dad8d3d296
remy <remy@passbolt.com>
2017-01-07 17:54:59 +0530

    PASSBOLT-1827 As a user I should be able to login with a passphrase longer than 50 char

--- tests/AP/regressions/PASSBOLT-1827_LoginWithPassphraseLongerThan50CharsTest.php
+++ tests/AP/regressions/PASSBOLT-1827_LoginWithPassphraseLongerThan50CharsTest.php
@@ -9,14 +9,13 @@ class PASSBOLT1827 extends PassboltSetupTestCase
 {
 
 	/**
-	 * @group no-saucelabs
-	 *
 	 * Scenario: As a user I should be able to login with a passphrase longer than 50 char
+	 *
 	 * Given    I register an account as John Doe
 	 * When     I complete the setup with a passphrase longer than 50 char
 	 * Then     I am able to login
 	 */
-	public function testPluginShouldStartAfterTwoSetup() {
+	public function testSetupAndLoginWithLongPassphrase() {
 		// Reset database at the end of test.
 		$this->resetDatabaseWhenComplete();
 
@@ -27,7 +26,10 @@ class PASSBOLT1827 extends PassboltSetupTestCase
 		// When I complete the setup with a passphrase longer than 50 char
 		$john['MasterPassword'] = 'As a AP I should be able to log in with a passphrase length that is longer than fifty character in length';
 		$this->goToSetup($john['Username']);
-		$this->completeSetupWithKeyGeneration($john);
+		$this->completeSetupWithKeyGeneration([
+			'username' => $john['Username'],
+			'masterpassword' =>  $john['MasterPassword']
+		]);
 
 		// Then I am able to login
 		$this->loginAs($john);
