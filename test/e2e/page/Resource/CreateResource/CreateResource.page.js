9f2e4d3028cc23b8fd999a465f82b118cb5ec04c
pierre <pierre@passbolt.com>
2025-04-01 08:09:45 +0200

    PB-39523 - WP6-7.7 Adapt selenium to work with new dialogs

--- test/e2e/page/Resource/CreateResource/CreateResource.page.js
+++ test/e2e/page/Resource/CreateResource/CreateResource.page.js
@@ -23,31 +23,35 @@ class CreateResourcePage {
    * define selectors using getter methods
    */
   get createPasswordPage() {
-    return $('.create-password-dialog.dialog-wrapper');
+    return $('.create-resource.dialog-wrapper');
   }
 
   get inputName() {
-    return $('#create-password-form-name');
+    return $('#resource-name');
   }
 
   get inputUri() {
-    return $('#create-password-form-uri');
+    return $('#resource-uri');
   }
 
   get inputUsername() {
-    return $('#create-password-form-username');
+    return $('#resource-username');
   }
 
   get inputPassword() {
-    return $('#create-password-form-password');
+    return $('#resource-password');
   }
 
   get inputDescription() {
-    return $('#create-password-form-description');
+    return $('#resource-note');
   }
 
   get submitButton() {
     return $('button[type=submit]');
+  }
+
+  get secretNoteTab() {
+    return $('#secret-note-tab');
   }
 
   /**
@@ -60,6 +64,8 @@ class CreateResourcePage {
     await this.inputUri.setValue(uri);
     await this.inputUsername.setValue(username);
     await this.inputPassword.setValue(password);
+    await this.secretNoteTab.click()
+    await this.secretNoteTab.waitForClickable();
     await this.inputDescription.setValue(description);
     await this.submitButton.waitForClickable();
     await this.submitButton.click();
